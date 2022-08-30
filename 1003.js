df6b73726e92c837e90b4fd175612b63afaeedd3
André <110499972+4ndreLeal@users.noreply.github.com>
2022-08-29 23:05:51 -0300

    simple sum

---
+++ 1003.js
@@ -0,0 +1,6 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+var A = parseInt(lines.shift());
+var B = parseInt(lines.shift());
+var sum = A + B;
+console.log("SOMA = " + sum);
